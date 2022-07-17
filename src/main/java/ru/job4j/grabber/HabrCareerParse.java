91e6db0996def6f8d1ae465f0682d7f2ef09d67a
BurlakovIvan <burlakovid@inbox.ru>
2022-07-17 19:01:21 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -12,22 +12,25 @@ import java.io.IOException;
 public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
-
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private static final int COUNT_PAGE = 5;
 
     public static void main(String[] args) throws IOException {
         HabrCareerDateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            Element dateElement = row.select(".basic-date").first();
-            var vacancyDate = dateTimeParser.parse(dateElement.attr("datetime"));
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s (%s) опубликовано: %s%n", vacancyName, link, vacancyDate);
-        });
+        for (int page = 0; page < COUNT_PAGE; page++) {
+            System.out.printf("%nДанные с %d страницы:%n", page + 1);
+            Connection connection = Jsoup.connect(String.format("%s%d", PAGE_LINK, page + 1));
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                Element dateElement = row.select(".basic-date").first();
+                var vacancyDate = dateTimeParser.parse(dateElement.attr("datetime"));
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.printf("%s (%s) опубликовано: %s%n", vacancyName, link, vacancyDate);
+            });
+        }
     }
 }

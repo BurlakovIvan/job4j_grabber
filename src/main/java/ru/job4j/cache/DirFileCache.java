788cdd3109b5b681abd1005975de21664c189ba5
BurlakovIvan <burlakovid@inbox.ru>
2022-08-02 16:02:40 +0300

    1. Реализация кеша на SoftReference [#1592]

--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -3,7 +3,6 @@ package ru.job4j.cache;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.*;
 import java.util.StringJoiner;
 
@@ -20,7 +19,8 @@ public class DirFileCache extends AbstractCache<String, String> {
     protected String load(String key) {
         if (validation(cachingDir, key)) {
             StringJoiner out = new StringJoiner(System.lineSeparator());
-            try (BufferedReader read = new BufferedReader(new FileReader(this.cachingDir + "\\" + key))) {
+            try (BufferedReader read = new BufferedReader(
+                    new FileReader(this.cachingDir + (!this.cachingDir.endsWith("/") ? "/" : "") + key))) {
                 read.lines().forEach(out::add);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -33,7 +33,7 @@ public class DirFileCache extends AbstractCache<String, String> {
     private boolean validation(String path, String file) {
         boolean rsl = true;
         validationDir(path);
-        Path filePath = Paths.get(path + "\\" + file);
+        Path filePath = Paths.get(path + (!path.endsWith("/") ? "/" : "") + file);
         if (!Files.exists(filePath)) {
             System.out.println("Не существует указанного файла!");
             rsl = false;
--- src/main/java/ru/job4j/cache/Emulator.java
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -7,23 +7,24 @@ public class Emulator {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Укажите кэшируемую директорию:");
         DirFileCache dirFileCache = new DirFileCache(scanner.nextLine());
-        int index;
-        do {
+        String index;
+        while (true) {
             System.out.println("Введи 1, чтобы загрузить содержимое файла в кэш");
             System.out.println("Введи 2, чтобы получить содержимое файла");
-            System.out.println("Введи любое число, чтобы выйти");
-            index = scanner.nextInt();
-            if (index == 1) {
+            System.out.println("Введи любой символ, чтобы выйти");
+            index = scanner.nextLine();
+            if ("1".equals(index)) {
                 System.out.println("Введи имя файла, который необходимо добавить в кэш");
-                String fileName = scanner.next();
+                String fileName = scanner.nextLine();
                 dirFileCache.load(fileName);
-            }
-            if (index == 2) {
+            } else if ("2".equals(index)) {
                 System.out.println("Введи имя файле, содержимое которого необходимо получить");
-                String fileName = scanner.next();
+                String fileName = scanner.nextLine();
                 System.out.println("Содержимое файла:");
                 System.out.println(dirFileCache.get(fileName));
+            } else {
+                break;
             }
-        } while (index > 0 && index < 3);
+        }
     }
 }
